caf37ef95dcb6019cf6436b6da8686e6f889dcaa
lucastandy <lucastitandy@gmail.com>
2024-01-05 10:19:38 -0300

    Criando formulário com React Native

--- App.js
+++ App.js
@@ -1,7 +1,15 @@
+// Importando useState: para adicionar estado a componentes funcioais
+import { useState } from 'react';
+// Incluindo os componentes do React Native
 import { ScrollView} from 'react-native';
-import {Container, Logo, ImageLogo, List, RowData, Bmi, Classification} from './app/src/styles/custom';
+// Incluindo os componentes CSS criado com styled componentes
+import {Container, Logo, ImageLogo, List, RowData, Bmi, Classification, InputForm, BtnSubmitForm, TxtSubmitForm} from './app/src/styles/custom';
 
 export default function App() {
+  // Criando variável de estado
+  const [height, setHeight] = useState('');
+  const [weight, setWeight] = useState('');
+  const [bmi, setBmi] = useState('');
   return (
     <ScrollView contentContainerStyle={{flexGrow: 1}}>
       <Container>
@@ -9,6 +17,20 @@ export default function App() {
         <Logo>
           <ImageLogo source={require('./app/assets/logo.png')} />
         </Logo>
+        {/* Criando o campo */}
+        <InputForm placeholder="Altura (ex.: 1.70)" value={height} onChangeText={text => setHeight(text)}/>
+        <InputForm placeholder="Peso (ex. 67.2)" value={weight} onChangeText={text => setWeight(text)}/>
+
+        {/* Criando o botão */}
+        <BtnSubmitForm>
+          <TxtSubmitForm>
+            Calcular
+          </TxtSubmitForm>
+        </BtnSubmitForm>
+        {/* Criando o campo */}
+        <InputForm placeholder="Seu IMC/BMI é" value={bmi} editable={false}/>
+
+
         {/* Criando a lista de classificação do IMC */}
         <List>
           <RowData>
